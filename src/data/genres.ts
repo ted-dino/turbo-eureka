652c96b9e9703fb44eddd27a6bf2af5829e43589
Ted Diño <teodorod@spacesium.com>
2023-08-16 16:47:57 +0800

    feat: fetch movie by genre

---
+++ src/data/genres.ts
@@ -0,0 +1,60 @@
+const genres = [
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 99,
+    name: "Dokumentarfilm",
+  },
+  {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 10751,
+    name: "Familie",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 36,
+    name: "Historie",
+  },
+  {
+    id: 27,
+    name: "Horror",
+  },
+  {
+    id: 10402,
+    name: "Musik",
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+  },
+  {
+    id: 878,
+    name: "Science Fiction",
+  },
+  {
+    id: 10770,
+    name: "TV-Film",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 37,
+    name: "Western",
+  },
+];
+
+export default genres
--- src/queryFns/movie.ts
+++ src/queryFns/movie.ts
@@ -1,16 +1,38 @@
 import axiosEndpoint from "@/lib/axiosEndpoint";
 import { MovieResult } from "@/types";
+import genres from "@/data/genres"
+
+const TMDB_MAX_PAGE = 500;
+const item = Math.floor(Math.random() * TMDB_MAX_PAGE) + 1;
 
 export const getRandomMovie = async () => {
-  const TMDB_MAX_PAGE = 500;
-  const item = Math.floor(Math.random() * TMDB_MAX_PAGE) + 1;
-  const randomMovie = await axiosEndpoint.get("/movie/popular", {
+  const randomMovie = await axiosEndpoint.get("/movie/top_rated", {
     params: {
       page: item,
       sort_by: "popularity.desc",
+      query: 'netflix',
     },
   });
   const data: MovieResult = await randomMovie.data;
 
   return randomMovie.data.results[0];
 };
+
+export const getRandomGenre =async () => {
+  const randomIndex = Math.floor(Math.random() * genres.length)
+  const genre = genres[randomIndex].name 
+
+  const randomMovieByGenre = await axiosEndpoint.get("/discover/movie", {
+    params: {
+      include_adult: 'false',
+      include_video: 'false',
+      page: item,
+      sort_by: 'popularity.desc',
+      with_genres: genre
+    }
+  })
+
+  const data  = await randomMovieByGenre.data
+
+  return data
+}
