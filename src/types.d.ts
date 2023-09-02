412fea5d07adac279ef1474d53baa7e134e3344d
Ted Diño <teodorod@spacesium.com>
2023-09-02 22:25:07 +0800

    refactor: extend types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,15 +1,19 @@
-export interface MovieResult {
+export interface Result {
     page:          number;
     results:       Result[];
     total_pages:   number;
     total_results: number;
 }
 
-export interface Result {
+export interface Item {
     adult:             boolean;
     backdrop_path:     string;
     genre_ids:         number[];
     id:                number;
+    first_air_date?:    Date;
+    name?:              string;
+    origin_country?:    OriginCountry[];
+    original_name?:     string;
     original_language: OriginalLanguage;
     original_title:    string;
     overview:          string;
@@ -20,6 +24,13 @@ export interface Result {
     video:             boolean;
     vote_average:      number;
     vote_count:        number;
+}
+
+export enum OriginCountry {
+    CA = "CA",
+    Jp = "JP",
+    Kr = "KR",
+    Us = "US",
 }
 
 export enum OriginalLanguage {
