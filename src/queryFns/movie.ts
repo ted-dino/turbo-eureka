089da686a97570189086f099fc66cbbc65940c5d
Ted Diño <teodorod@spacesium.com>
2023-09-02 22:35:36 +0800

    refactor: clean up code

--- src/queryFns/movie.ts
+++ src/queryFns/movie.ts
@@ -1,11 +1,9 @@
 import axiosEndpoint from "@/lib/axiosEndpoint";
-import { MovieResult } from "@/types";
-import genres from "@/data/genres";
-
-const TMDB_MAX_PAGE = 500;
-const item = Math.floor(Math.random() * TMDB_MAX_PAGE) + 1;
 
 export const getRandomMovie = async () => {
+  const TMDB_MAX_PAGE = 500;
+  const item = Math.floor(Math.random() * TMDB_MAX_PAGE) + 1;
+
   const randomMovie = await axiosEndpoint.get("/movie/top_rated", {
     params: {
       page: item,
@@ -16,24 +14,4 @@ export const getRandomMovie = async () => {
   });
 
   return randomMovie.data.results[0];
-};
-
-export const getRandomGenre = async () => {
-  const randomIndex = Math.floor(Math.random() * genres.length);
-  const genre = genres[randomIndex].name;
-
-  const randomMovieByGenre = await axiosEndpoint.get("/discover/movie", {
-    params: {
-      include_adult: false,
-      page: item + randomIndex,
-      sort_by: "popularity.desc",
-      with_genres: genre,
-      language: "en-US",
-    },
-  });
-
-  const data: MovieResult = await randomMovieByGenre.data;
-  const movieList = { ...data, genre };
-
-  return movieList;
 };
