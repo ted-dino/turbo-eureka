9aed2b95bcf9f636b034b34ae29e350343213853
Ted Diño <teodorod@spacesium.com>
2023-09-03 15:18:46 +0800

    feat: add footer

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -2,24 +2,46 @@ const menu = [
   {
     id: 1,
     name: "Home",
-    link: "",
+    link: "#",
   },
   {
     id: 2,
     name: "Movies",
-    link: "",
+    link: "/movies",
   },
   {
     id: 3,
     name: "Series",
-    link: "",
+    link: "#",
   },
   {
     id: 4,
     name: "My List",
-    link: "",
+    link: "#",
   },
 ];
 
+export default menu;
 
-export default menu
+export const footer = [
+  {
+    id: 1,
+    name: "Home",
+    link: "#",
+  },
+  {
+    id: 2,
+    name: "Contact",
+    link: "#",
+  },
+  {
+    id: 3,
+    name: "Privacy Policy",
+    link: "#",
+  },
+  {
+    id: 4,
+    name: "Terms of Service",
+    link: "#",
+  },
+];
