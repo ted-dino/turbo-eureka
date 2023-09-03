7d8df3fb925f3ee9a26dbe312817bdc9c8739554
Ted Diño <teodorod@spacesium.com>
2023-09-03 17:48:14 +0800

    feat: finish home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
-import FeaturedMovie from "@/components/custom-ui/FeaturedMovie";
+import SlidersWrapper from "@/components/custom-ui/Item/Wrapper";
+import ItemDialog from "@/components/custom-ui/Item/Dialog";
+import FeatMovieWrapper from "@/components/custom-ui/Wrapper";
 
 export default function Home() {
-  return <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
-    <FeaturedMovie/>
-    </main>;
+  const homeEndpoints = [
+    {
+      title: "Popular Movies",
+      params: "/movie/popular",
+    },
+    {
+      title: "Top Rated Movies",
+      params: "/movie/top_rated",
+    },
+    {
+      title: "Popular Series",
+      params: "/tv/popular",
+    },
+    {
+      title: "Top Rated Series",
+      params: "/tv/top_rated",
+    },
+  ];
+  return (
+    <main className="relative">
+      <FeatMovieWrapper endpoint="/movie/top_rated" />
+      <SlidersWrapper page="home" options={homeEndpoints} />
+      <ItemDialog />
+    </main>
+  );
 }
