274aa9bb1e5c1f15e97f26d82a991a21e2af63e8
Ted Diño <teodorod@spacesium.com>
2023-09-03 17:47:48 +0800

    feat: add movies page

---
+++ src/app/movies/page.tsx
@@ -0,0 +1,51 @@
+import Wrapper from "@/components/custom-ui/Wrapper";
+import SlidersWrapper from "@/components/custom-ui/Item/Wrapper";
+import ItemDialog from "@/components/custom-ui/Item/Dialog";
+
+export default function page() {
+  const moviesEndpoints = [
+    {
+      title: "Action",
+      params: "28",
+    },
+    {
+      title: "Comedy",
+      params: "35",
+    },
+    {
+      title: "Documentary",
+      params: "99",
+    },
+    {
+      title: "Family",
+      params: "Family",
+    },
+    {
+      title: "History",
+      params: "10751",
+    },
+    {
+      title: "Horror",
+      params: "27",
+    },
+    {
+      title: "Science Fiction",
+      params: "878",
+    },
+    {
+      title: "War",
+      params: "10752",
+    },
+    {
+      title: "Western",
+      params: "37",
+    },
+  ];
+  return (
+    <main>
+      <Wrapper endpoint="/movie/popular" />
+      <SlidersWrapper page="movies" options={moviesEndpoints} />
+      <ItemDialog />
+    </main>
+  );
+}
