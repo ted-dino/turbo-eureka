aa64736a1ab24adb3f2cfe9f2645e96de9f1df74
Ted Diño <teodorod@spacesium.com>
2023-09-03 15:06:46 +0800

    feat: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navigation from "@/components/ui/navigation";
 import QueryProvider from "@/QueryProvider";
+import Footer from "@/components/custom-ui/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -23,6 +24,7 @@ export default function RootLayout({
           <div className="shadow-banner relative h-screen lg:h-[140vh]">
             <Navigation />
             {children}
+            <Footer />
           </div>
         </body>
       </html>
