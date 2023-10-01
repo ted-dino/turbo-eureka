b8c840f455fb03211693b9bad90bdf299c79ec01
Ted Diño <teodorod@spacesium.com>
2023-10-01 18:31:50 +0800

    feat: make component more dynamic

--- src/components/custom-ui/SimilarList.tsx
+++ src/components/custom-ui/SimilarList.tsx
@@ -5,9 +5,10 @@ import Link from "next/link";
 
 interface Props {
   similar: Similar;
+  route?: string;
 }
 
-export default function SimilarList({ similar }: Props) {
+export default function SimilarList({ similar, route = "movies" }: Props) {
   return (
     <section className="lg:container lg:mx-auto mb-20 px-5 lg:px-0">
       <h2 className="mt-10 mb-5 text-xl lg:text-4xl">You may also like</h2>
@@ -15,18 +16,20 @@ export default function SimilarList({ similar }: Props) {
         {similar.results.map((similar) => (
           <li key={similar.id} className="mb-5">
             <Link
-              href={`/movies/info/${similar.id}/${normalizeURL(similar.title)}`}
+              href={`/${route ? route : "tv-series"}/info/${
+                similar.id
+              }/${normalizeURL(similar.title ? similar.title : similar.name!)}`}
               className="relative grid h-[150px] w-[150px] md:h-[330px] md:w-[217px]"
             >
               <Image
                 src={getBackdropImg(similar.poster_path)}
-                alt={similar.title}
+                alt={similar.title ? similar.title : similar.name!}
                 fill
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 className="mb-5 object-cover rounded-md w-auto h-auto"
               />
               <span className="relative block mt-[100%] md:mt-[154%] text-sm leading-tight">
-                {similar.title}
+                {similar.title ? similar.title : similar.name!}
               </span>
             </Link>
           </li>
