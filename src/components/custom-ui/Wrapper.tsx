53be312d21a6f77366191421d3b784589c75f841
Ted Diño <teodorod@spacesium.com>
2023-10-03 17:06:12 +0800

    refactor: delete components

--- src/components/custom-ui/Wrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import FeaturedItem from "./FeaturedMovie";
-import { getRandomMovie } from "@/queryFns/movie";
-
-export default function Wrapper({ endpoint }: { endpoint: string }) {
-  return <FeaturedItem queryFn={() => getRandomMovie(endpoint)} />;
-}
