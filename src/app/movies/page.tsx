02789c73505fa9755b474c695d708e02cb13ee86
Ted Diño <teodorod@spacesium.com>
2023-10-03 18:08:14 +0800

    refactor: adjust code

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,51 +1,65 @@
-import Wrapper from "@/components/custom-ui/Wrapper";
 import SlidersWrapper from "@/components/custom-ui/Item/Wrapper";
-import ItemDialog from "@/components/custom-ui/Item/Dialog";
+import FeaturedMovie from "@/components/custom-ui/Movies/FeaturedMovie";
+import MovieDialog from "@/components/custom-ui/Movies/MovieDialog";
 
-export default function Movies() {
+type Props = {
+  searchParams: Record<string, string> | null | undefined;
+};
+
+export default function Movies({ searchParams }: Props) {
+  const modal = searchParams && searchParams.selectedShow;
   const moviesEndpoints = [
     {
       title: "Action",
       params: "28",
+      type: "movie",
     },
     {
       title: "Comedy",
       params: "35",
+      type: "movie",
     },
     {
       title: "Documentary",
       params: "99",
+      type: "movie",
     },
     {
       title: "Family",
       params: "10751",
+      type: "movie",
     },
     {
       title: "History",
       params: "36",
+      type: "movie",
     },
     {
       title: "Horror",
       params: "27",
+      type: "movie",
     },
     {
       title: "Science Fiction",
       params: "878",
+      type: "movie",
     },
     {
       title: "War",
       params: "10752",
+      type: "movie",
     },
     {
       title: "Western",
       params: "37",
+      type: "movie",
     },
   ];
   return (
     <main>
-      <Wrapper endpoint="/movie/popular" />
+      <FeaturedMovie />
       <SlidersWrapper page="movies" options={moviesEndpoints} />
-      <ItemDialog />
+      {modal !== undefined && <MovieDialog />}
     </main>
   );
 }
