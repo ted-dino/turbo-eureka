273e53dc46a70e0cc60f2def6686d8d92b8a9d95
Ted Diño <teodorod@spacesium.com>
2023-10-06 20:05:33 +0800

    feat: add file for media endpoints

---
+++ src/data/endpoints.ts
@@ -0,0 +1,203 @@
+export const homeEndpoints = [
+  {
+    title: "Popular Movies",
+    params: "/movie/popular",
+    type: "movie",
+  },
+  {
+    title: "Top Rated Movies",
+    params: "/movie/top_rated",
+    type: "movie",
+  },
+  {
+    title: "Popular Series",
+    params: "/tv/popular",
+    type: "series",
+  },
+  {
+    title: "Top Rated Series",
+    params: "/tv/top_rated",
+    type: "series",
+  },
+];
+
+export const moviesEndpoints = [
+  {
+    params: "28",
+    title: "Action",
+    type: "movie",
+  },
+  {
+    params: "12",
+    title: "Adventure",
+    type: "movie",
+  },
+  {
+    params: "16",
+    title: "Animation",
+    type: "movie",
+  },
+  {
+    params: "35",
+    title: "Comedy",
+    type: "movie",
+  },
+  {
+    params: "80",
+    title: "Crime",
+    type: "movie",
+  },
+  {
+    params: "99",
+    title: "Documentary",
+    type: "movie",
+  },
+  {
+    params: "18",
+    title: "Drama",
+    type: "movie",
+  },
+  {
+    params: "10751",
+    title: "Family",
+    type: "movie",
+  },
+  {
+    params: "14",
+    title: "Fantasy",
+    type: "movie",
+  },
+  {
+    params: "36",
+    title: "History",
+    type: "movie",
+  },
+  {
+    params: "27",
+    title: "Horror",
+    type: "movie",
+  },
+  {
+    params: "10402",
+    title: "Music",
+    type: "movie",
+  },
+  {
+    params: "9648",
+    title: "Mystery",
+    type: "movie",
+  },
+  {
+    params: "10749",
+    title: "Romance",
+    type: "movie",
+  },
+  {
+    params: "878",
+    title: "Science Fiction",
+    type: "movie",
+  },
+  {
+    params: "10770",
+    title: "TV Movie",
+    type: "movie",
+  },
+  {
+    params: "53",
+    title: "Thriller",
+    type: "movie",
+  },
+  {
+    params: "10752",
+    title: "War",
+    type: "movie",
+  },
+  {
+    params: "37",
+    title: "Western",
+    type: "movie",
+  },
+];
+
+export const seriesEndpoints = [
+  {
+    params: "10759",
+    title: "Action & Adventure",
+    type: "series",
+  },
+  {
+    params: "16",
+    title: "Animation",
+    type: "series",
+  },
+  {
+    params: "35",
+    title: "Comedy",
+    type: "series",
+  },
+  {
+    params: "80",
+    title: "Crime",
+    type: "series",
+  },
+  {
+    params: "99",
+    title: "Documentary",
+    type: "series",
+  },
+  {
+    params: "18",
+    title: "Drama",
+    type: "series",
+  },
+  {
+    params: "10751",
+    title: "Family",
+    type: "series",
+  },
+  {
+    params: "10762",
+    title: "Kids",
+    type: "series",
+  },
+  {
+    params: "9648",
+    title: "Mystery",
+    type: "series",
+  },
+  {
+    params: "10763",
+    title: "News",
+    type: "series",
+  },
+  {
+    params: "10764",
+    title: "Reality",
+    type: "series",
+  },
+  {
+    params: "10765",
+    title: "Sci-Fi & Fantasy",
+    type: "series",
+  },
+  {
+    params: "10766",
+    title: "Soap",
+    type: "series",
+  },
+  {
+    params: "10767",
+    title: "Talk",
+    type: "series",
+  },
+  {
+    params: "10768",
+    title: "War & Politics",
+    type: "series",
+  },
+  {
+    params: "37",
+    title: "Western",
+    type: "series",
+  },
+];
