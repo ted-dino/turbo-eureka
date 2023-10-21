9a01134b788d90f6095204090b4c74868a9ffc2b
Ted Diño <teodorod@spacesium.com>
2023-10-21 11:55:51 +0800

    fix: prevent user from accessing showLogin route

--- src/components/custom-ui/Common/AuthContainer.tsx
+++ src/components/custom-ui/Common/AuthContainer.tsx
@@ -1,10 +1,18 @@
+import { redirect } from "next/navigation";
 import AuthPanel from "../AuthPanel";
+import { cookies } from "next/headers";
 
 type Props = {
-  searchParams: Record<string, string> | null | undefined;
+  searchParams: Record<string, string>;
 };
 
 export default function AuthContainer({ searchParams }: Props) {
+  const isLoggedIn = cookies().get("tedflix.session-token");
   const showLogin = searchParams && searchParams.showLogin;
-  return <>{showLogin && <AuthPanel showLogin={showLogin} />}</>;
+
+  if (isLoggedIn && showLogin) {
+    redirect("/");
+  }
+
+  return <>{showLogin && !isLoggedIn && <AuthPanel showLogin={showLogin} />}</>;
 }
